1e1415d9adb52595ce817cf2c32ab0f4af37c9e4
zhangzhe <zhez6@illinois.edu>
2019-04-23 20:22:44 -0500

    Delete irrelevant code

--- main.py
+++ main.py
@@ -2,90 +2,9 @@ import numpy as np
 import time
 import matplotlib.pyplot as plt
 import collections
-np.random.seed(20190604)
-# class Game:
-#     def __init__(self, b_acc, b_esc, skill_1_up_acc, skill_1_down_esc, skill_2_down_acc, skill_2_up_esc, num_players):
-#         self.b_acc = b_acc
-#         self.b_esc = b_esc
-#         self.skill_1_up_acc = skill_1_up_acc
-#         self.skill_1_down_esc = skill_1_down_esc
-#         self.skill_2_up_esc = skill_2_up_esc
-#         self.skill_2_down_acc = skill_2_down_acc
-#         self.num_players = num_players
-#         if self.b_acc + self.skill_1_up_acc > 1:
-#             print('Skill 1 accuracy increase too much!')
-#             raise Exception('Please reduce skill 1 accuracy increase.')
-#         if self.b_esc - self.skill_1_down_esc < 0:
-#             print('Skill 1 escape decrease too much!')
-#             raise Exception('Please reduce skill 1 escape decrease')
-#         if self.b_acc - self.skill_2_down_acc < 0:
-#             print('Skill 2 accuracy decrease too much!')
-#             raise Exception('Please reduce skill 2 accuracy decrease.')
-#         if self.b_esc + self.skill_2_down_acc > 1:
-#             print('Skill 2 escape increase too much!')
-#             raise Exception('Please reduce skill 2 escape increase.')
-#
-#     def set_target(self, num_players):
-#         res = np.random.randint(0, num_players, num_players)
-#         while np.sum(res == np.arange(num_players)) != 0:
-#             res = np.random.randint(0, num_players, num_players)
-#         return res
-#
-#     def choose_skill(self, skill):
-#         if skill == 0:
-#             self.acc = self.b_acc
-#             self.esc = self.b_esc
-#         elif skill == 1:
-#             self.acc = self.b_acc + self.skill_1_up_acc
-#             self.esc = self.b_esc - self.skill_1_down_esc
-#         elif skill == 2:
-#             self.acc = self.b_acc - self.skill_2_down_acc
-#             self.esc = self.b_esc + self.skill_2_up_esc
-#         else:
-#             raise Exception('Please input a valid choice.')
-#
-#     def hit_a_round(self, num_players):
-#         target = self.set_target(num_players)
-#         accuracy = np.array([self.acc] + [self.b_acc] * (num_players - 1))
-#         escape = np.array([self.esc] + [self.b_esc] * (num_players - 1))
-#         hit = np.random.binomial(1, accuracy)
-#         hit = target[hit != 0]
-#         hit_count = np.zeros(num_players, dtype=int)
-#         count = np.unique(hit, return_counts=True)
-#         hit_count[count[0]] = count[1]
-#         escape_needed = escape ** hit_count
-#         random_escape = np.random.rand(num_players)
-#         survivors = random_escape < escape_needed
-#         if np.sum(survivors) == 0:
-#             return True, 0
-#         else:
-#             return survivors[0], np.sum(survivors)
-#
-#     def start_a_game(self):
-#         num_players = self.num_players
-#         while True:
-#             main_player, num_players = self.hit_a_round(num_players)
-#             if not main_player:
-#                 return False
-#             if num_players == 0 or num_players == 1:
-#                 return True
-#
-#
-# G = Game(b_acc=0.5, b_esc=0.1, skill_1_up_acc=0.3, skill_1_down_esc=0.05,
-#          skill_2_up_esc=0.3, skill_2_down_acc=0.05, num_players=100)
-# G.choose_skill(2)
-# count_ = 0
-# tic = time.time()
-# for i in range(100000):
-#     res = G.start_a_game()
-#     if res:
-#         count_ += 1
-# toc = time.time()
-# print(toc - tic)
-# print(count_/100000)
 
 
-class Game2:
+class Game:
     def __init__(self, basic_acc, acc_change, def_change, other_acc, acc_std, num_players,
                  head_percentage, head_mean_damage, head_std_damage, body_mean_damage, body_std_damage):
         """
@@ -210,7 +129,7 @@ class Game2:
 
 
 num_game = 10000
-g = Game2(0.5, 0.1, 0.05, 0.5, 0.05, 100, 0.3, 0.9, 0.06, 0.45, 0.03)
+g = Game(0.5, 0.1, 0.05, 0.5, 0.05, 100, 0.3, 0.9, 0.06, 0.45, 0.03)
 g.choose_skill(0)
 count = 0
 round = []
